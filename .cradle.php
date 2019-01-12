f6eaac2d9f409d6dd289ccaa17032d6d9fd7b272
Christian Blanquera <cblanquera@openovate.com>
2019-01-13 05:37:02 +0800

    adding login attempts, slow down DDOS

--- .cradle.php
+++ .cradle.php
@@ -6,3 +6,5 @@ require_once __DIR__ . '/package/events.php';
 require_once __DIR__ . '/src/events.php';
 require_once __DIR__ . '/src/controller.php';
 require_once __DIR__ . '/package/helpers.php';
+
+$this->preprocess(include(__DIR__ . '/src/bootstrap/attempts.php'));
