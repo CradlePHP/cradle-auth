d507c98af054a4da6d63f50b6bbc754a433cfc38
Christian Blanquera <cblanquera@openovate.com>
2019-01-15 12:03:02 +0800

    precurser for admin settings and SSO

--- .cradle.php
+++ .cradle.php
@@ -3,8 +3,11 @@
  * This file is part of a Custom Package.
  */
 require_once __DIR__ . '/package/events.php';
-require_once __DIR__ . '/src/events.php';
-require_once __DIR__ . '/src/controller.php';
 require_once __DIR__ . '/package/helpers.php';
+require_once __DIR__ . '/src/events.php';
+require_once __DIR__ . '/src/controller/auth.php';
+require_once __DIR__ . '/src/controller/admin.php';
 
+
+//bootstrap
 $this->preprocess(include(__DIR__ . '/src/bootstrap/attempts.php'));
